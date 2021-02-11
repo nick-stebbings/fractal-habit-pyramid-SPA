3177703287cc0552199ea8116ed6e2f5bdaa40c1
Nick Stebb <nickolarse@riseup.net>
2021-02-11 16:31:36 +1300

    fix text scrolling behind nav

--- src/view/components/ui/ListCard.jsx
+++ src/view/components/ui/ListCard.jsx
@@ -5,33 +5,33 @@ import Icon from "./Icon.jsx"
 
 const ListCard = {
   view: ({ attrs }) => (
-    <div class="h-24 list-card-container">
-      <div class="flex items-start">
-                  <div class="label-row w-16 flex-shrink-0 flex flex-col text-right leading-none">
-                    <span class="h-8 name-label">
+    <div class="list-card-container">
+      <div class="list-card">
+                  <div class="label-col">
+                    <span class="name-label">
                       { "Name:" }
                     </span>
-                    <span class="h-8 stats-label">
+                    <span class="stats-label">
                       { "Stats:" }
                     </span>
                   </div>
-                  <div class="flex-shrink-1 flex flex-col leading-none">
+                  <div class="details-col">
                     <h2 class="h-8 font-bold text-xl text-bold">{ "My Habit Name" }</h2>
-                    <div class="stats-row h-8 flex-row flex justify-items-center">
+                    <div class="stats-row">
                       <span>
-                        <span class="habit-streak text-3xl text-balance-success text-sans">
+                        <span class="habit-streak text-balance-success">
                           { "3" }
                         </span>
-                        <span class="habit-streak-desc text-2xl">
+                        <span class="habit-streak-desc">
                           { "Days of Streak" }
                         </span>
                       </span>
 
                       <span>
-                        <span class="habit-days-tracked text-3xl text-blue-300 text-sans">
+                        <span class="habit-days-tracked text-blue-300">
                           { "99" }
                         </span>
-                        <span class="habit-days-tracked-desc text-2xl">
+                        <span class="habit-days-tracked-desc">
                           { "Days Tracked" }
                         </span>
                       </span>
